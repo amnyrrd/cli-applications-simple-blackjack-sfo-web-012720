ad834e789437abc75f6ea90f61da8c10518a5a26
Aaron Minyard <amnyrrd@gmail.com>
2020-01-21 07:14:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -46,9 +46,10 @@ def hit?(total)
 end
 
 def invalid_command
-  puts "Please enter a valid command"
-  prompt_user
-  get_user_input
+  if get_user_input !"h" || !"s"
+puts "Please enter a valid command"
+prompt_user
+get_user_input
 end
 
 #####################################################
